b81217273eeba21c8ae6923bdc9a7b98da413535
Eniola John-Phillips <eniola_johnphillips@yahoo.com>
2021-07-06 00:10:07 +0300

    first commit

---
+++ test_homework9.py
@@ -0,0 +1,8 @@
+# from unittest import TestCase
+#
+#
+# class Test(TestCase):
+#     def test_example1(self):
+#         expected =
+#         result =
+#         self.assertEqual(expected, result)
