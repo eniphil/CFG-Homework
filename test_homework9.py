22596fa8ba97aca8a50325e17077d051791a7b5b
Anyi Guo <yanniey.guo@gmail.com>
2021-07-11 18:06:29 +0100

    add anyi's comment for homework 8 and 9

--- test_homework9.py
+++ test_homework9.py
@@ -1,8 +1,18 @@
-# from unittest import TestCase
-#
-#
-# class Test(TestCase):
-#     def test_example1(self):
-#         expected =
-#         result =
-#         self.assertEqual(expected, result)
+from unittest import TestCase, mock
+from Homework8 import account
+
+
+class Test(TestCase):
+    def test_example1(self):
+        # valid withdrawl
+        expected = 20
+        with mock.patch("builtins.input", return_value=80): # this line mocks (i.e. pretends) that the input for "How much you'd like to withdraw" is 80.
+            result = account()
+        self.assertEqual(expected, result)
+
+
+    def test_example2(self):
+        # invalid withdrawl
+        with mock.patch("builtins.input", return_value=150): # this line pretends that the input for "How much you'd like to withdraw" is 150.
+            result = account()
+        self.assertEqual("Insufficient funds", account())
